5a286b3eed074d347ff309a22b73aecc2d23a619
yahong2018 <491337961@qq.com>
2021-06-22 09:18:39 +0800

    adding the omrom plc gathering project

---
+++ omron/src/main/java/com/zhxh/imms/omron/OmronApplication.java
@@ -0,0 +1,13 @@
+package com.zhxh.imms.omron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmronApplication.class, args);
+	}
+
+}
---
+++ omron/src/test/java/com/zhxh/imms/omron/OmronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhxh.imms.omron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
